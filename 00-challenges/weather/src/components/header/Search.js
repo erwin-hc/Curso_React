f53f654e83d0e079cffdc4419982ef07299e82a6
erwin <erwin.stein@gmail.com>
2024-06-04 16:50:43 -0300

    04/06/24

--- 00-challenges/weather/src/components/header/Search.js
+++ 00-challenges/weather/src/components/header/Search.js
@@ -1,28 +1,68 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useEffect, useState } from 'react'
 import { MdOutlineMyLocation } from "react-icons/md";
-import useGeolocation from '../../hooks/useGeolocation'
+import { useGeolocation } from '../../hooks/useGeolocation'
+import { City } from "country-state-city";
 
 export function Search() {
   const [query, setQuery] = useState("")
-  const { lat, lng, getPosition, getCity, cityName , stateName } = useGeolocation();
-  const localName = cityName || stateName ? `${cityName}, ${stateName}` : ""
+  const [suggestions, setSuggestions] = useState([]);
+
+  const { cityName, isLoading, getPosition } = useGeolocation();
+
+  const possibleValues = City.getAllCities().map((c) => c.name);
+
+  function handleClick() {
+    getPosition()
+    setSuggestions([])
+    setQuery(cityName)
+  }
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setQuery(value);
+
+    if (value.length > 3) {
+      const filteredSuggestions = possibleValues
+        .filter((suggestion) =>
+          suggestion.toLowerCase().includes(value.toLowerCase())
+        )
+        .sort((a, b) => a.localeCompare(b));
+      setSuggestions(filteredSuggestions);
+    } else {
+      setSuggestions([]);
+    }
+  };
+
+  const handleSuggestionClick = (value) => {
+    setQuery(value);
+    setSuggestions([]);
+  };
   
   useEffect(()=>{
-    
-  },[])
-
+    setQuery(cityName)
+  },[cityName])
+ 
   return (
     <div className='flex items-center justify-center w-full max-w-5xl mx-auto mt-8'>
       <div className='relative w-full h-10'>
-      <input value={query} onChange={(e)=> setQuery(e.target.value)} placeholder='Search...' className='w-full h-10 text-white border-none rounded-full outline-none pl-14 bg-blue-300/20 oxygen-bold'></input>
-      <MdOutlineMyLocation onClick={() => {
-        getPosition()
-        getCity()
-        setQuery(localName)
-        console.log(lat)
-        console.log(lng)
-        }} className='absolute text-3xl text-white cursor-pointer top-1 left-3 opacity-30 hover:opacity-70'/>
-      </div>
+      <input value={isLoading ? "Loading ..." : query}
+      onChange={handleInputChange}
+      placeholder='Search...' 
+      className='text-lg w-full h-10 text-blue-100 border-none rounded-full outline-none pl-14 bg-blue-300/20 oxygen-bold'></input>
+      <MdOutlineMyLocation onClick={handleClick}
+      className='absolute text-3xl text-blue-100 cursor-pointer top-1 left-3 opacity-30 hover:opacity-70'/>
+      { suggestions.length > 0 &&
+          <div className='absolute w-full rounded-2xl  bg-blue-900/95 top-12 left-0 flex flex-col items-start py-2 max-h-[240px] overflow-y-auto'>
+              <ul className='w-full'>
+              {suggestions.map((suggestion, index) => (
+                <li className='hover:bg-blue-400/20 hover:font-bold cursor-pointer w-full text-lg list-none pl-14  text-blue-100 oxygen-regular'key={index} onClick={() => handleSuggestionClick(suggestion)}>
+                  {suggestion}
+                </li>
+              ))}
+            </ul> 
+          </div>
+      }
+      </div> 
     </div>
   )
 }
--- 00-challenges/weather/src/hooks/useGeolocation.js
+++ 00-challenges/weather/src/hooks/useGeolocation.js
@@ -1,36 +1,44 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-export default function useGeolocation() {  
-    const [lat, setLat] = useState(null)
-    const [lng, setLng] = useState(null)
-    const [cityName, setCityName] = useState(null)
-    const [stateName, setStateName] = useState(null)
+export function useGeolocation() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [position, setPosition] = useState({});
+    const [error, setError] = useState(null);
+    const [cityName, setCityName] = useState("");
 
-    if (!navigator.geolocation) {
-      console.log('Sem localização')
+    async function fetchCityName(lat, lng) {      
+        const YOUR_PRIVATE_TOKEN = `pk.bbc27fd22d82fd79345d3a03093148df`
+        const URL = `https://us1.locationiq.com/v1/reverse.php?key=${YOUR_PRIVATE_TOKEN}&lat=${lat}&lon=${lng}&format=json`
+
+        const res = await fetch(URL)
+        const data = await res.json()
+          if (data.address) {
+            setCityName(data.address.city)
+            // setStateName(data.address.state)
+          }        
+    } 
+       
+    function getPosition() {
+        if (!navigator.geolocation)
+            return setError("Your browser does not support geolocation");
+      
+          setIsLoading(true);
+          navigator.geolocation.getCurrentPosition(
+            (pos) => {
+              setPosition({
+                lat: pos.coords.latitude,
+                lng: pos.coords.longitude
+              });
+              fetchCityName(pos.coords.latitude, pos.coords.longitude)
+              setIsLoading(false);
+            },
+            (error) => {
+              setError(error.message);
+              setIsLoading(false);
+            }
+          );
     }
 
-    function getPosition() {     
-      navigator.geolocation.getCurrentPosition((position)=>{
-      const { coords } = position;
-      setLat(coords.latitude)
-      setLng(coords.longitude)
-    })    
-    }
-     
-    async function getCity() {
-    const YOUR_PRIVATE_TOKEN = `pk.bbc27fd22d82fd79345d3a03093148df`
-    const URL = `https://us1.locationiq.com/v1/reverse.php?key=${YOUR_PRIVATE_TOKEN}&lat=${lat}&lon=${lng}&format=json`
-
-    const res = await fetch(URL)
-    const data = await res.json()
-      if (data.address) {
-        setCityName(data.address.city)
-        setStateName(data.address.state)
-      }
-    }
-
-    useEffect(()=> getPosition(),[])
-    
-    return { lat, lng, getPosition, getCity , cityName, stateName}
-  }
+    return { isLoading, position, error, cityName, getPosition}
+  
+}
